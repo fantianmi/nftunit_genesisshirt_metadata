e519beb6d81d8e8887f6bdb933cb7cf2fffa003c
fantianmi@163.com <fantianmi@163.com>
2022-06-20 17:53:13 +0800

    s

---
+++ Application/GenesisShirt/Info/info.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 15-6-24
+ * Time: 下午3:09
+ * @author 郑钟良<zzl@ourstu.com>
+ */
+return array(
+    //模块名
+    'name' => 'GenesisShirt',
+    //别名
+    'alias' => 'GenesisShirt',
+    //版本号
+    'version' => '1.0.0',
+    //是否商业模块,1是，0，否
+    'is_com' => 0,
+    //是否显示在导航栏内？  1是，0否
+    'show_nav' => 0,
+    //模块描述
+    'summary' => '',
+    //开发者
+    'developer' => '',
+
+    'icon' => 'globe',
+    //开发者网站
+    'website' => ''
+);
