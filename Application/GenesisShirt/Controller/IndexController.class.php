15b08707adc600e9dd9fba53b4afe98de1cd3c93
fantianmi@163.com <fantianmi@163.com>
2022-06-22 08:53:58 +0800

    s

--- Application/GenesisShirt/Controller/IndexController.class.php
+++ Application/GenesisShirt/Controller/IndexController.class.php
@@ -19,14 +19,12 @@ class IndexController extends Controller
     function erc1155()
     {
         $Metadata = new MetadataModel();
-//        $image = "https://gateway.pinata.cloud/ipfs/QmS2aPSfLzrEpj9MJQwcwPaxZq5dB6arsieWbjoKDeL5D8";
-        $image = "https://themta.mypinata.cloud/ipfs/QmTwpqR5NjH1McZrMGp5qrwMzhKQ9ENygfnMkiPNN77zjg/2949.png";
+        $image = "https://gateway.pinata.cloud/ipfs/QmS2aPSfLzrEpj9MJQwcwPaxZq5dB6arsieWbjoKDeL5D8";
+//        $image = "https://themta.mypinata.cloud/ipfs/QmTwpqR5NjH1McZrMGp5qrwMzhKQ9ENygfnMkiPNN77zjg/2949.png";
         $animation_url = "https://gateway.pinata.cloud/ipfs/QmWaHPdkqpG8eu3M6K3QRJz5rvRLhZgmwF5kPGTFSZnKCj/1155.gif";
         $link = "http://tshit.nftunit.site";
         $name = "NFTUNIT GENESIS SHIRT";
-        $description = "THE FIRST IRL COLLECTION OF NFTUNIT COMMUNITY. 
-                        DESIGN by 0xDomi 
-                        Holding a Genesis Shirt nft qualifies you to redeem shirt merch in here (tshit.nftunit.site）";
+        $description = "THE FIRST IRL COLLECTION OF NFTUNIT COMMUNITY. DESIGN by 0xDomi Holding a Genesis Shirt nft qualifies you to redeem shirt merch in here (tshit.nftunit.site）";
         $data = $Metadata->create($image, $animation_url, $link, $name, $description);
         $this->json($data);
     }
